115769342cc594406d6132a637a1d693b84a55da
Dan Barkwell <dbarkwell@pelism.com>
2026-06-17 07:47:50 -0400

    UI fixes

--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ function App() {
         {isAppSettingsOpen && <AppSettings />}
         <MenuSidebar />
         <PanelGroup orientation="horizontal">
-          <Panel defaultSize={225} minSize={225} maxSize={300} className="relative z-20 border-r border-border">
+          <Panel defaultSize={225} minSize={225} maxSize={750} className="relative z-20 border-r border-border">
             <Sidebar />
           </Panel>
           
